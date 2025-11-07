0cfff518ce56e59e6bacdb9beac36452e9163bea
vjp-ivanbg <ibuesog01@informatica.iesvalledeljerteplasencia.es>
2025-11-07 04:21:32 +0100

    Add files via upload

---
+++ Tema 4/ejercicio7/src/main/java/ejercicio7/Ejercicio7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicio7;
+
+/**
+ *
+ * @author bueso
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
